9a94c74bffcd3e7e228016c985e01a0935b54a1e
christoffersilverson <christoffer@silverson.email>
2019-01-07 13:25:48 +0100

    kommentarer

--- 2019-01-07/3b.sh
+++ 2019-01-07/3b.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -e "$1" ]; then
 	while read -r line; do
-	printf "$line \n"
-	printf "\n"
-	printf "\n"
+	printf "$line \n" #print the line
+	printf "\n" #print a space
+	printf "\n" #print another space, this can be removed.
 	done < "$1"
 else
 	echo "error: file does not exist."
