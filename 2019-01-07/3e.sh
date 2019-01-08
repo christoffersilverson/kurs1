11f1501aaf555b0e32004b1fa5037a593a04c8a5
christoffer <christoffer@silverson.email>
2019-01-08 03:22:18 -0800

    changed magic to lucky

--- 2019-01-07/3e.sh
+++ 2019-01-07/3e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function isMagic() {
+function isLucky() {
 	#loop over every num in the array
 	var=0
 	arr2=("$@")
@@ -7,17 +7,17 @@ function isMagic() {
 		let "var=var+j"
 	done
 	if [ $var == "7" ]; then
-		return 0 #return success if magic number is found
+		return 0 #return success if lucky number is found
 	elif [ ${#var} -gt 1 ]; then #if length of number(string) is above 1, ie: number is above 10
 		m=0
 		while [ $m -lt ${#var} ]; do n[$m]=${var:$m:1}; m=$((m+1));done #converts a string $var into array of characters
-		isMagic "${n[@]}" #call isMagic recursively
+		isLucky "${n[@]}" #call isLucky recursively
 	else
-		return 1 #return fail if number isnt magic
+		return 1 #return fail if number isnt lucky
 	fi
 }
-for (( x=1000 ; x <= 10000; x++ )); do #loop 1000 -> 10000
+for (( x=1000 ; x <= 10000000; x++ )); do #loop 1000 -> 10000
 	l=0
 	while [ $l -lt ${#x} ]; do y[$l]=${x:$l:1};  l=$((l+1));done #converts a string $x in to array of characters
-	isMagic "${y[@]}" && echo "$x is magic!" #call function isMagic and echo $x on successful return
+	isLucky "${y[@]}" && echo "$x is lucky!" #call function isLucky and echo $x on successful return
 done
