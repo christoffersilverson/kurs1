ab1d1533bb72e6ae8fac000359fad6a05dc198d5
christoffersilverson <christoffer@silverson.email>
2019-01-08 09:34:23 +0100

    finished script 3e and added comments

--- 2019-01-07/3e.sh
+++ 2019-01-07/3e.sh
@@ -4,27 +4,21 @@ function isMagic() {
 	#loop over every num in the array
 	var=0
 	arr2=("$@")
-	#echo $arr2
-	for j in "${arr2[@]}" ; do
-		#echo "var: $var"
-		echo "j: $j"
+	for j in "${arr2[@]}" ; do #sum all the digits in the number
 		let "var=var+j"
-		echo "var: $var"
 	done
-	if [ var == 7 ]; then
-		echo "value is magic!"
-		return 1
+	if [ $var == "7" ]; then
+		return 0 #return success if magic number is found
 	elif [ ${#var} -gt 1 ]; then
-		#var=$(echo $var | fold -w1)
-		#echo "recursing"
-		#isMagic "${var[@]}" #rekursiv galenskap
+		m=0
+		while [ $m -lt ${#var} ]; do n[$m]=${var:$m:1}; m=$((m+1));done #converts a string $var into array of characters
+		isMagic "${n[@]}" #call isMagic recursively
 	else
-		return 0
+		return 1 #return fail if number isnt magic
 	fi
 }
 for (( x=1000 ; x <= 10000; x++ )); do #loopa 1000 -> 10000
 	l=0
-	while [ $l -lt ${#x} ]; do y[$l]=${x:$l:1};  l=$((l+1));done
-	echo "arr: ${y[@]}"
-	isMagic "${y[@]}" #is magic?
+	while [ $l -lt ${#x} ]; do y[$l]=${x:$l:1};  l=$((l+1));done #converts a string $x in to array of characters
+	isMagic "${y[@]}" && echo "$x is magic!"
 done
